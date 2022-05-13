462288c5fffa74e62553c787545a63b3ceeb962c
NIKITA <NIKITA@HP>
2022-05-13 11:43:25 +0530

    commit first

---
+++ TNS/src/day54/HashsetDemo.java
@@ -0,0 +1,20 @@
+package day54;
+
+import java.util.HashSet;
+
+public class HashsetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee1 e1 = new Employee1(1,"Ramesh",15000);
+		Employee1 e2 = new Employee1(1,"Ramesh",15000);
+		
+		HashSet hs=new HashSet();
+		hs.add(e1);
+		System.out.println(hs.contains(e2));
+		System.out.println(e1.hashCode());
+		System.out.println(e2.hashCode());
+	}
+
+}
+
---
+++ TNS/src/day54/HashsetDemo2.java
@@ -0,0 +1,20 @@
+package day54;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class HashsetDemo2 {
+	
+	    public static void main(String[] args)
+	    {
+	        HashSet<String> hs = new HashSet<String>();
+	 
+	        hs.add("Hello");
+	        hs.add("Hii");
+	        hs.add("Bye");
+	 
+	        System.out.println("HashSet elements : " + hs);
+	    }
+	
+}
+
