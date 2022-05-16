2701a6ac37a35fdab4b86b7ea8d7220d62961555
NIKITA <NIKITA@HP>
2022-05-16 10:38:59 +0530

    first commit

---
+++ JDBC_Connection/src/jdbc/StatementDemo.java
@@ -0,0 +1,33 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Properties;
+
+
+public class StatementDemo {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/b3tns","root","Mysql@01");
+		String url = "jdbc:mysql://localhost:3306/b3tns";
+		Properties p= new Properties();
+		p.put("user", "root");
+		p.put("password", "Mysql@01");
+		Connection con = DriverManager.getConnection(url, p);
+		Statement st = con.createStatement();
+		String query1 = "create table student( studentId int primary key, studentName varchar(20), studentMarks int)";
+		int i = st.executeUpdate(query1);
+		int j = st.executeUpdate("create table student1(studentId int primary key, studentName varchar(20), studentMarks int");
+		System.out.println("No of rows affected: " +i);//0
+		System.out.println("No of rows affected: " +j);//0
+		System.out.println("Table created Sucessfully");
+		st.close();//Communication
+		con.close();//connection
+
+	}
+
+}
+
